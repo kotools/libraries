d1216bc83f8b5342429a732bb372e4790e5bc438
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-16 13:30:47 +0100

    feat(csv-#47): add the NotBlankString.csv property

---
+++ csv/src/main/kotlin/kotools/csv/Path.kt
@@ -0,0 +1,41 @@
+package kotools.csv
+
+import kotools.shared.Project.Csv
+import kotools.shared.SinceKotools
+import kotools.types.NotBlankString
+import kotools.types.toNotBlankString
+
+/** Representation of a path pointing to a CSV file. */
+@JvmInline
+@SinceKotools(Csv, "2.3")
+public value class CsvPath
+private constructor(private val value: NotBlankString) {
+    internal companion object {
+        val extension: NotBlankString by lazy(
+            ".csv".toNotBlankString()::getOrThrow
+        )
+
+        infix fun of(value: NotBlankString): Result<CsvPath> = value
+            .takeIf { "$it" != "$extension" }
+            ?.let { "$it.csv" }
+            ?.toNotBlankString()
+            ?.getOrThrow()
+            ?.let(::CsvPath)
+            ?.let(Result.Companion::success)
+            ?: Result.failure(
+                IllegalArgumentException(
+                    "Given path shouldn't equal the CSV extension."
+                )
+            )
+    }
+
+    /** Returns this path as a [String]. */
+    override fun toString(): String = "$value"
+}
+
+/**
+ * Returns this string as a [CsvPath], or an [IllegalArgumentException] if this
+ * string equals the `.csv` extension.
+ */
+@SinceKotools(Csv, "2.3")
+public val NotBlankString.csv: Result<CsvPath> get() = CsvPath of this
---
+++ csv/src/test/kotlin/kotools/csv/PathTest.kt
@@ -0,0 +1,30 @@
+package kotools.csv
+
+import kotools.assert.assertEquals
+import kotools.assert.assertFailsWith
+import kotools.assert.assertNotNull
+import kotools.assert.assertTrue
+import kotools.types.toNotBlankString
+import org.junit.jupiter.api.Test
+
+class PathTest {
+    @Test
+    fun notBlankString_csv_should_pass_with_a_NotBlankString_other_than_the_extension() {
+        val file = "file"
+        file.toNotBlankString()
+            .getOrThrow()
+            .csv
+            .getOrThrow()
+            .toString() assertEquals "$file${CsvPath.extension}"
+    }
+
+    @Test
+    fun notBlankString_csv_should_fail_with_a_NotBlankString_that_equals_the_extension() {
+        val result: Result<CsvPath> = CsvPath.extension.csv
+        assertFailsWith<IllegalArgumentException>(result::getOrThrow)
+            .message
+            .assertNotNull()
+            .isNotBlank()
+            .assertTrue()
+    }
+}
