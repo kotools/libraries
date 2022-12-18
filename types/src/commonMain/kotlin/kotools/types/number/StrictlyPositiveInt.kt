786a6b568ccc49ef575019b1484acdbe80b654e4
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-18 15:50:50 +0100

    feat(types-#132): add Int.toStrictlyPositiveInt function

--- types/src/commonMain/kotlin/kotools/types/number/StrictlyPositiveInt.kt
+++ types/src/commonMain/kotlin/kotools/types/number/StrictlyPositiveInt.kt
@@ -2,10 +2,28 @@ package kotools.types.number
 
 import kotools.shared.Project.Types
 import kotools.shared.SinceKotools
+import kotools.types.aStrictlyPositiveNumber
+import kotools.types.shouldBe
+import kotools.types.toSuccessfulResult
 import kotlin.jvm.JvmInline
 
 /** Representation of positive integers excluding [zero][ZeroInt]. */
 @JvmInline
 @SinceKotools(Types, "1.1")
 public value class StrictlyPositiveInt
-private constructor(override val value: Int) : NonZeroInt, PositiveInt
+private constructor(override val value: Int) : NonZeroInt, PositiveInt {
+    internal companion object {
+        infix fun of(value: Int): Result<StrictlyPositiveInt> = value
+            .takeIf { it > 0 }
+            ?.toSuccessfulResult(::StrictlyPositiveInt)
+            ?: Result.failure(value shouldBe aStrictlyPositiveNumber)
+    }
+}
+
+/**
+ * Returns this integer as a [StrictlyPositiveInt], or an
+ * [IllegalArgumentException] if this integer is [negative][NegativeInt].
+ */
+@SinceKotools(Types, "1.1")
+public fun Int.toStrictlyPositiveInt(): Result<StrictlyPositiveInt> =
+    StrictlyPositiveInt of this
---
+++ types/src/commonTest/kotlin/kotools/types/number/StrictlyPositiveIntTest.kt
@@ -0,0 +1,31 @@
+package kotools.types.number
+
+import kotools.assert.assertEquals
+import kotools.assert.assertFailsWith
+import kotools.assert.assertNotNull
+import kotools.assert.assertTrue
+import kotlin.random.Random
+import kotlin.random.nextInt
+import kotlin.test.Test
+
+class StrictlyPositiveIntTest {
+    @Test
+    fun int_toStrictlyPositiveInt_should_pass_with_a_strictly_positive_Int() {
+        val value: Int = Random.nextInt(1..Int.MAX_VALUE)
+        value.toStrictlyPositiveInt()
+            .getOrThrow()
+            .value assertEquals value
+    }
+
+    @Test
+    fun int_toStrictlyPositiveInt_should_fail_with_a_negative_Int() {
+        val result: Result<StrictlyPositiveInt> = Random
+            .nextInt(Int.MIN_VALUE..0)
+            .toStrictlyPositiveInt()
+        assertFailsWith<IllegalArgumentException>(result::getOrThrow)
+            .message
+            .assertNotNull()
+            .isNotBlank()
+            .assertTrue()
+    }
+}
