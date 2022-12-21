c31c0a29e2579cbf4959af25da637736d5d5b81f
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-21 22:24:13 +0100

    feat(types-#132): update serializer of PositiveInt

--- types/src/commonMain/kotlin/kotools/types/number/PositiveInt.kt
+++ types/src/commonMain/kotlin/kotools/types/number/PositiveInt.kt
@@ -1,8 +1,17 @@
 package kotools.types.number
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotools.shared.Project.Types
 import kotools.shared.SinceKotools
+import kotools.types.Package
+import kotools.types.serialization.toIntSerialDescriptor
+import kotools.types.text.NotBlankString
+import kotools.types.text.toNotBlankString
 
 /** Representation of positive integers including [zero][ZeroInt]. */
 @Serializable(PositiveIntSerializer::class)
@@ -20,5 +29,19 @@ public fun Int.toPositiveInt(): Result<PositiveInt> = when {
     else -> Result.failure(this shouldBe aPositiveNumber)
 }
 
-internal object PositiveIntSerializer :
-    AnyIntSerializer<PositiveInt>(Int::toPositiveInt)
+internal object PositiveIntSerializer : KSerializer<PositiveInt> {
+    override val descriptor: SerialDescriptor = "${Package.number}.PositiveInt"
+        .toNotBlankString()
+        .map(NotBlankString::toIntSerialDescriptor)
+        .getOrThrow()
+
+    override fun serialize(encoder: Encoder, value: PositiveInt): Unit =
+        encoder.encodeInt(value.value)
+
+    override fun deserialize(decoder: Decoder): PositiveInt {
+        val value: Int = decoder.decodeInt()
+        return value.toPositiveInt()
+            .getOrNull()
+            ?: throw SerializationException(value shouldBe aPositiveNumber)
+    }
+}
